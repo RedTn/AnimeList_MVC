73bb3b3dda07f5f04ed1540559a6f8c7b06f7544
RedTn <redmond.tran@gmail.com>
2015-05-22 22:12:41 -0700

    reinsert authorize

--- MyWebApp/Controllers/AnimeListController.cs
+++ MyWebApp/Controllers/AnimeListController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace MyWebApp.Controllers
 {
+    [Authorize]
     public class AnimeListController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
